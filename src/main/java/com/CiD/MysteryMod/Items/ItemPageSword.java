9494a23b841ce8b19f7c2a5689434e691d072fc0
CiD-WestBrooklyn <Cidsbaum@gmail.com>
2015-01-26 20:05:20 +0100

    Added the first page

---
+++ src/main/java/com/CiD/MysteryMod/Items/ItemPageSword.java
@@ -0,0 +1,35 @@
+package com.CiD.MysteryMod.Items;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import com.CiD.MysteryMod.MysteryMain;
+import com.CiD.MysteryMod.Pages.EnumBookType;
+
+public class ItemPageSword extends ItemPage{
+
+	public ItemPageSword(String tooltip, EnumBookType bookType, int PageNumber,
+			String PageTitle, String name) {
+		super(tooltip, bookType, 1, "The mystery of the sword", name);
+	}
+
+
+	@Override
+		public ItemStack onItemRightClick(ItemStack stack, World world,
+				EntityPlayer player) {
+		if(!world.isRemote){
+			if(player.inventory.hasItem(MysteryMain.book_mystery)){
+				for(int i = 0; i < 35;i++){
+					if(player.inventory.getStackInSlot(i) != null&&player.inventory.getStackInSlot(i).getItem() != null&&player.inventory.getStackInSlot(i).getItem() == MysteryMain.book_mystery){
+						ItemMystBook book = (ItemMystBook)player.inventory.getStackInSlot(i).getItem();
+						book.addPageToBook(MysteryMain.page_sword, player.inventory.getStackInSlot(i), player);
+					}
+				}
+			}
+		}
+			return stack;
+		}
+	
+	
+}
